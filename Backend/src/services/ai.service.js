603ecc90f3dd6524f0cc34b9c653ceb7655274f3
Kunal Deshmukh <deshmukh1207@gmail.com>
2026-03-17 21:53:44 +0530

    zod schema 2

--- Backend/src/services/ai.service.js
+++ Backend/src/services/ai.service.js
@@ -41,4 +41,24 @@ async function generateInterviewReport({ resume, selfDescription, jobDescription
 }
 
 
+
+async function generatePdfFromHtml(htmlContent) {
+    const browser = await puppeteer.launch()
+    const page = await browser.newPage();
+    await page.setContent(htmlContent, { waitUntil: "networkidle0" })
+
+    const pdfBuffer = await page.pdf({
+        format: "A4", margin: {
+            top: "20mm",
+            bottom: "20mm",
+            left: "15mm",
+            right: "15mm"
+        }
+    })
+
+    await browser.close()
+
+    return pdfBuffer
+}
+
 module.exports = invokeGeminiAi
