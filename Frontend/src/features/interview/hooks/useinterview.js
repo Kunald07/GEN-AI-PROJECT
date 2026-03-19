c9ff7ec59a655f9bc7c74545be1709a7e6617f70
Kunal Deshmukh <deshmukh1207@gmail.com>
2026-03-19 21:40:18 +0530

    use Interview Hook & Report Management Logic

---
+++ Frontend/src/features/interview/hooks/useinterview.js
@@ -0,0 +1,63 @@
+import {getAllInterviewReports, generateInterviewReport, getInterviewReportById} from "../services/interview.api"
+import { useContext } from "react"
+import { InterviewContext } from "../interview.context"
+
+export const useInterview = () => {
+
+    const context = useContext(InterviewContext)
+
+    if(!context) {
+        throw new Error("useInterview must be used within an InterviewProvider")
+    }
+
+    const {laoding, setLoading, report, setReport, reports, setReports} = context
+
+    const generateReport = async ({jobDescription, selfDescription, resumeFile}) => {
+        setLoading(true)
+        let response = null
+        try {
+            const response = await generateInterviewReport({jobDescription, selfDescription, resumeFile})
+            setReport(response.interviewReport)
+        } catch (error) {
+            console.log(error);
+        } finally {
+            setLoading(false)
+        }
+
+            return response.interviewReport
+    }
+
+        const getReportById = async (interviewId) => {
+        setLoading(true)
+        let response = null
+        try {
+            response = await getInterviewReportById(interviewId)
+            setReport(response.interviewReport)
+        } catch (error) {
+            console.log(error)
+        } finally {
+            setLoading(false)
+        }
+        return response.interviewReport
+    }
+
+    const getReports = async () => {
+        setLoading(true)
+        let response = null
+        try {
+            response = await getAllInterviewReports()
+            setReports(response.interviewReports)
+        } catch (error) {
+            console.log(error)
+        } finally {
+            setLoading(false)
+        }
+
+        return response.interviewReports
+    }
+
+    return{laoding, report, reports, generateReport, getReportById, getReports}
+
+
+
+}
